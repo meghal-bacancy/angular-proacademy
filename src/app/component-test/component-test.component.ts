b8d5dd9baeda491d82ccfda9dc1a26c7f6fd8a8b
Meghal <meghal.shah@bacancy.com>
2025-04-26 23:12:10 +0530

    Component-Selector

---
+++ src/app/component-test/component-test.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-component-test',
+  // selector: '[app-component-test]',
+  // selector: '.app-component-test',
+  templateUrl: './component-test.component.html',
+  styleUrls: ['./component-test.component.scss']
+})
+export class ComponentTestComponent {
+
+}
